e1dc6f06578ee55030c1cd345e64a88169aa4630
Rafael Pires <rafaelbrpires@gmail.com>
2022-06-29 15:56:37 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+
+
+function displaynum(n1){
+    urna.text1.value = urna.text1.value+n1;
+   
+
+    const lula = 157
+    const bolso = 171
+
+    if(document.urna.text1.value == lula){
+        console.log("lulinha");}
+    else if(document.urna.text1.value == bolso){
+        console.log("Bolsonabo");}
+    else {
+        console.log("Candidato não existe")}
+    }
+
